21b20cfab7b8e4b57345914edcb3595deece3c87
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-01-17 09:14:31 -0800

    added login and password inputs with a handleSubmit preventing default

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import Navbar from '../../comps/Navbar/Navbar';
-import container from '../../styles/Container.module.css';
+import React from "react";
+import Navbar from "../../comps/Navbar/Navbar";
+import container from "../../styles/Container.module.css";
 
 const Login = () => {
-	return (
-		<div className={container.container}>
-			<Navbar />
-			<h1>Login Component</h1>
-		</div>
-	);
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <div className={container.container}>
+      <Navbar />
+      <h1>Login Component</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input></input>
+      </form>
+      <form onSubmit={handleSubmit}>
+        <label>Password</label>
+        <input></input>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
