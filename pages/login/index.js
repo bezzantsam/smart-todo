80c77d624e5c36afda2087ea7ac58af272ece001
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-01-17 11:16:01 -0500

    finalized file structure.

---
+++ pages/login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../comps/Navbar/Navbar';
+
+const Login = () => {
+	return (
+		<div>
+			<Navbar />
+			<h1>Login Component</h1>
+		</div>
+	);
+};
+
+export default Login;
---
+++ pages/signup/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../comps/Navbar/Navbar';
+
+const SignUp = () => {
+	return (
+		<div>
+			<Navbar />
+			<h1>SignUp Component</h1>
+		</div>
+	);
+};
+
+export default SignUp;
